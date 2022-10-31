d94b1ab6a911b9050406074d63aa985650954ebb
waelll05 <92450588+waelll05@users.noreply.github.com>
2022-10-31 12:08:27 +0100

    Update list.py

--- list.py
+++ list.py
@@ -4,7 +4,7 @@ List1.reverse()
 print("the reversed list is", List1)
 
 List2 = [91, 67, 120, 34, 76, 54, 78, 78, 56, 64, 345]
-List.sort()
+List2.sort()
 print ("the sorted list is" , List2)
 
 List3 = []
