5460c5c70c9d143ed59dc8d137832cc819b7d89a
waelll05 <92450588+waelll05@users.noreply.github.com>
2022-10-31 12:12:01 +0100

    Update list.py

--- list.py
+++ list.py
@@ -7,8 +7,4 @@ List2 = [91, 67, 120, 34, 76, 54, 78, 78, 56, 64, 345]
 List2.sort()
 print ("the sorted list is" , List2)
 
-List3 = []
-List3 = List1.copy()
-print("List3 =", List3)
-indexvalue = List2[2:6]
-print("the index value are" , indexvalue)
+
